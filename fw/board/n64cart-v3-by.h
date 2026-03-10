2f3b5fe648f1e0b424cf1ec8a9e448ab4b19e496
Claudiu Zissulescu <claudiu.zissulescu-ianculescu@oracle.com>
2026-03-10 10:50:13 +0200

    fw: add v3-by board header for BY25Q128AS

---
+++ fw/board/n64cart-v3-by.h
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+// -----------------------------------------------------
+// NOTE: THIS HEADER IS ALSO INCLUDED BY ASSEMBLER SO
+//       SHOULD ONLY CONSIST OF PREPROCESSOR DIRECTIVES
+// -----------------------------------------------------
+
+// This header may be included by other board headers as "boards/pico.h"
+
+#ifndef _BOARDS_N64CART_H
+#define _BOARDS_N64CART_H
+
+// For board detection
+#define N64CART_RP2040_V3
+
+// --- UART ---
+#ifndef PICO_DEFAULT_UART
+#define PICO_DEFAULT_UART 0
+#endif
+#ifndef PICO_DEFAULT_UART_TX_PIN
+#define PICO_DEFAULT_UART_TX_PIN 28
+#endif
+#ifndef PICO_DEFAULT_UART_RX_PIN
+#define PICO_DEFAULT_UART_RX_PIN 29
+#endif
+
+// --- LED ---
+#ifndef PICO_DEFAULT_LED_PIN
+#define PICO_DEFAULT_LED_PIN 25
+#endif
+
+#ifndef PICO_LED_WS2812
+#define PICO_LED_WS2812 1
+#endif
+
+// --- FLASH ---
+
+// BY25Q128AS uses Winbond-compatible boot flow and 3-byte addressing.
+#define PICO_BOOT_STAGE2_CHOOSE_W25Q080 1
+
+#ifndef DISABLE_FLASH_ADDR_32
+#define DISABLE_FLASH_ADDR_32 1
+#endif
+
+#ifndef PICO_FLASH_SPI_CLKDIV
+#define PICO_FLASH_SPI_CLKDIV 2
+#endif
+
+#ifndef PICO_FLASH_SIZE_BYTES
+#define PICO_FLASH_SIZE_BYTES (2 * 1024 * 1024)
+#endif
+
+#ifndef PICO_RP2040_B0_SUPPORTED
+#define PICO_RP2040_B0_SUPPORTED 0
+#endif
+
+#endif
